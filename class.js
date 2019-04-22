a46963385be35f0d58fe5756875187539efab41e
sungtae <munnt72@hotmail.com>
2019-04-23 07:47:14 +0900

    prototype : Complete

---
+++ class.js
@@ -0,0 +1,7 @@
+var kim = new Person('kim',10,20);
+kim.sum = function(){
+    return 'this : '+(this.first+this.second);
+}
+var lee = new Person('lee',10,10);
+console.log("kim.sum()", kim.sum())
+console.log("lee.sum()", lee.sum())
