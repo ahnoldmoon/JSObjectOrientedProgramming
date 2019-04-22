71863dadce9a31f76dff7d53e61cac79cae8f18f
sungtae <munnt72@hotmail.com>
2019-04-23 07:55:22 +0900

    class : Complete

--- class.js
+++ class.js
@@ -1,7 +1,13 @@
-var kim = new Person('kim',10,20);
-kim.sum = function(){
-    return 'this : '+(this.first+this.second);
+class Person{
+
 }
-var lee = new Person('lee',10,10);
-console.log("kim.sum()", kim.sum())
-console.log("lee.sum()", lee.sum())
+
+var kim = new Person();
+console.log('kim', kim);
+
+// kim.sum = function(){
+//     return 'this : '+(this.first+this.second);
+// }
+// var lee = new Person('lee',10,10);
+// console.log("kim.sum()", kim.sum())
+// console.log("lee.sum()", lee.sum())
